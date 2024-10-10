6fe8652bf8544d452e7614f604e50bdeeeca9e8d
RodrigoBit <rodrigomurtabittencourt@gmail.com>
2024-10-10 18:59:57 -0300

    Atualizacao Css, HTML e Js

--- trivia/script.js
+++ trivia/script.js
@@ -1,1 +1,11 @@
-.
+const botaoCorreto = document.getElementById('RespostaCorreta');
+
+botaoCorreto.addEventListener('click', function(){
+    this.classList.toggle('botao-clicado');
+});
+
+const botoes = document.querySelectorAll('button');
+
+botoes.addEventListener('click', function(){
+    this.classList.toggle('botao-clicado-errado');
+});
