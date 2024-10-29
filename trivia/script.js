c6d9981cc2ddb9dd8a1f18c375daf22e38ad0ac9
RodrigoBit <rodrigomurtabittencourt@gmail.com>
2024-10-29 11:50:37 -0300

    Nova pergunta aberta adicionada e tamanho das fontes e botões melhorados

--- trivia/script.js
+++ trivia/script.js
@@ -20,28 +20,44 @@ botoesErrados.forEach((botao) => {
   });
 });
 // Seleciona a caixa de texto que receberá a resposta
-const box = document.querySelector(".box");
+const box = document.querySelectorAll('.box');
+const box1 = document.querySelector("#box1");
+const box2 = document.querySelector("#box2");
 
 // Seleciona o botao que enviará a resposta
-const enviar = document.querySelector(".enviar");
+const enviar1 = document.querySelector("#enviar1");
+const enviar2 = document.querySelector("#enviar2");
 
 // Limpa a seleção
 // function limpar(params) {
 //   box.value = '';
 // }
 
-function checar(params) {
-  if (box.value.toUpperCase() === "BRASIL") {
-    box.classList.toggle("respostaAbertaCorreta");
+function checar1() {
+  if (box1.value.toUpperCase() === "BRASIL" || box1.value.toUpperCase() === "BRAZIL") {
+    box1.classList.toggle("respostaAbertaCorreta", true);
     console.log("Resposta Correta");
   } else {
-    box.classList.toggle("respostaAbertaErrada");
+    box1.classList.toggle("respostaAbertaErrada", true);
     console.log("Resposta Errada");
   }
   // limpar();
 }
-box.addEventListener("click", function (params) {
-  this.classList.toggle("respostaAbertaCorreta", false);
-  this.classList.toggle("respostaAbertaErrada", false);
+function checar2() {
+  if (box2.value.toUpperCase() === "C") {
+    box2.classList.toggle("respostaAbertaCorreta", true);
+    console.log("Resposta Correta");
+  } else {
+    box2.classList.toggle("respostaAbertaErrada", true);
+    console.log("Resposta Errada");
+  }
+  // limpar();
+}
+box.forEach(b => {
+  b.addEventListener("click", function () {
+    this.classList.toggle("respostaAbertaCorreta", false);
+    this.classList.toggle("respostaAbertaErrada", false);
+  });
 });
-enviar.addEventListener("click", checar);
+enviar1.addEventListener("click", checar1);
+enviar2.addEventListener("click", checar2);
